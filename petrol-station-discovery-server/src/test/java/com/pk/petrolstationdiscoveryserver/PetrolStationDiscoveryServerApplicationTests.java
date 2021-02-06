5c8d063440b462effc3c4c0462d2a8eca29d121c
slawomirbuczek <slawekbuczek210@gmail.com>
2021-02-06 16:27:11 +0100

    Added eureka server

---
+++ petrol-station-discovery-server/src/test/java/com/pk/petrolstationdiscoveryserver/PetrolStationDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pk.petrolstationdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetrolStationDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
